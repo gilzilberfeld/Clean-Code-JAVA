d4f4d1af283ffe7ce268cb8588b7ea9c9dc89674
gilzilberfeld <gil.zilberfeld@gmail.com>
2022-07-07 12:01:34 +0300

    remove from git

---
+++ src/main/java/testingil/cleancode/CBI/Navigator.java
@@ -0,0 +1,10 @@
+package testingil.cleancode.CBI;
+
+// We want to build a navigator
+// It can tell us the distance between where we are and we want to go
+// It reports the distance in km
+// We can tell it to drive there
+// We have a dependency on 3rd party distance calculator that reports the distance in miles
+
+public class Navigator {
+}
